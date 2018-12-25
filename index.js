4dedda49d141096081ec7bc047c56122bc6f64a2
Jon Schlinkert <jon.schlinkert@sellside.com>
2018-12-25 00:47:45 -0500

    clean up

--- index.js
+++ index.js
@@ -3,141 +3,88 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-const File = require('./file');
 
-const readdir = (baseDir, options = {}, cb) => {
+const readdir = (baseDir, options, cb) => {
   if (typeof options === 'function') {
     cb = options;
-    options = {};
+    options = null;
   }
 
-  if (!cb) {
-    return util.promisify(readdir)(baseDir, options);
+  if (typeof cb !== 'function') {
+    return readdir.promise(baseDir, options);
   }
 
-  let files = [];
-  let noop = () => true;
-  let toPath = file => (options.absolute || !file.relative) ? file.path : file.relative;
-  let push = file => {
-    if (file.keep !== false) {
-      files.push(options.path ? toPath(file) : file);
-    }
-  };
+  let opts = { withFileTypes: true, ...options };
+  let filter = opts.filter || (() => true);
+  let result = [];
+  let push = file => result.push(opts.withFileTypes ? file : file.path);
 
-  let base = path.resolve(baseDir);
-  let onFile = options.onFile || noop;
-  let onMatch = options.onMatch || noop;
-  let onFolder = options.onFolder || noop;
-  let filter = options.filter || (() => options.filesOnly !== true);
-
-  const walk = async (folder, parent, next) => {
-    folder.parent = folder.relative = parent;
-    folder.isDirectory = () => true;
-    let isMatch = filter(folder) === true;
-    let dirname = folder.path = path.join(base, parent);
-    let recurse = await onMatch(folder);
-
-    await onFolder(folder);
-    push(folder);
-
-    if (folder.recurse === false || options.recurse === false) {
-      recurse = false;
+  const walk = (folder, next) => {
+    if (folder.path !== baseDir) {
+      if (filter(folder) === true) {
+        push(folder);
+      } else {
+        next(null, result);
+        return;
+      }
     }
 
-    if (dirname !== base && recurse === false) {
-      next(null, files);
-      return;
-    }
-
-    fs.readdir(dirname, (err, paths) => {
+    fs.readdir(folder.path, { withFileTypes: true }, (err, paths) => {
       if (err) {
-        err.file = folder;
         next(err);
         return;
       }
 
       let len = paths.length;
       if (len === 0) {
-        next(null, files);
+        next(null, result);
         return;
       }
 
-      paths.forEach(basename => {
-        let file = new File(base, dirname, basename);
+      paths.forEach(file => {
+        file.path = path.join(folder.path, file.name);
 
-        fs.lstat(file.path, async(err, stat) => {
-          file.stat = stat;
-
-          if (err) {
-            err.file = file;
-            next(err);
-            return;
-          }
-
-          if (stat.isDirectory()) {
-            walk(file, path.join(parent, basename), err => {
-              if (err) {
-                err.file = file;
-                next(err);
-                return;
-              }
-
-              if (--len === 0) {
-                next(null, files);
-              }
-            });
-          } else {
-            file.parent = parent;
-            file.relative = path.join(parent, basename);
-            file.isDirectory = () => false;
-            await onMatch(file);
-            await onFile(file);
-            push(file);
+        if (file.isDirectory()) {
+          walk(file, err => {
+            if (err) {
+              next(err);
+              return;
+            }
 
             if (--len === 0) {
-              next(null, files);
+              next(null, result);
             }
+          });
+        } else {
+          if (filter(file) === true) {
+            push(file);
           }
-        });
+
+          if (--len === 0) {
+            next(null, result);
+          }
+        }
       });
     });
   }
 
-  let file = new File(base, path.dirname(base), path.basename(base));
-  return walk(file, '', cb);
-}
+  let file = new fs.Dirent(baseDir);
+  file.path = path.resolve(file.name);
+  file.name = path.basename(baseDir);
+  file.isDirectory = () => true;
+  walk(file, cb);
+};
 
-// class File {
-//   constructor(base, dirname, basename) {
-//     this.base = base;
-//     this.dirname = dirname;
-//     this.basename = basename;
-//     this.path = path.join(dirname, basename);
-//   }
-
-//   get folder() {
-//     return this._parent ? this._parent.folder : this.basename;
-//   }
-
-//   get rest() {
-//     if (this._rest) return this._rest;
-//     return (this._rest = this.path.slice(this.base.length + 1));
-//   }
-//   get segs() {
-//     if (this._segs) return this._segs;
-//     return (this._segs = this.rest.split(path.sep));
-//   }
-//   get folder() {
-//     if (this._folder) return this._folder;
-//     return (this._folder = this.segs[0]);
-//   }
-
-//   set relative(val) {
-//     this._relative = val;
-//   }
-//   get relative() {
-//     return this._relative || path.relative(this.base, this.path);
-//   }
-// }
+readdir.promise = (...args) => {
+  return new Promise((resolve, reject) => {
+    readdir(...args, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+};
 
 module.exports = readdir;
