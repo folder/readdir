3d354790da290df9fc0a0e7436ffc337c2ec123e
Jon Schlinkert <jon.schlinkert@sellside.com>
2021-04-07 17:42:20 -0400

    update benchmarks, deps

--- bench/index.js
+++ bench/index.js
@@ -2,10 +2,11 @@
 
 const readdirEnhanced = require('@mrmlnc/readdir-enhanced');
 const readdirp = require('readdirp');
+const { fdir: Fdir } = require('fdir');
 const Composer = require('composer');
 const composer = new Composer();
 const bench = require('./bench');
-const readdir = require('../lib/basic');
+const readdir = require('../lib/async');
 
 const enhanced = (dir, options) => {
   const files = [];
@@ -23,11 +24,29 @@ const enhanced = (dir, options) => {
   });
 };
 
+const fdir = (dir, options) => {
+  return new Fdir()
+    .withFullPaths()
+    .withMaxDepth(0)
+    .withDirs()
+    .crawl(dir)
+    .withPromise();
+};
+
+const fdirRecursive = (dir, options) => {
+  return new Fdir()
+    .withFullPaths()
+    .withDirs()
+    .crawl(dir)
+    .withPromise();
+};
+
 const log = files => console.log(files.sort().length);
 
 composer.task('readdir', () => {
   return bench('readdir')
     .add('@folder/readdir', () => readdir(__dirname, { recursive: false }))
+    .add('fdir', () => fdir(__dirname))
     .add('readdir-enhanced', () => enhanced(__dirname))
     .add('readdirp', () => readdirp.promise(__dirname, { depth: 1 }))
     .run();
@@ -36,6 +55,7 @@ composer.task('readdir', () => {
 composer.task('recursive', () => {
   return bench('readdir - recursive')
     .add('@folder/readdir', () => readdir(__dirname, { recursive: true }))
+    .add('fdir', () => fdirRecursive(__dirname))
     .add('readdir-enhanced', () => enhanced(__dirname, { deep: true }))
     .add('readdirp', () => readdirp.promise(__dirname))
     .run();
